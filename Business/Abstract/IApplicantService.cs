885f607cb89430f296fe54ffa4bdeb1a36f4e891
İnan Akalın <111204187+inanakalin@users.noreply.github.com>
2024-02-18 02:49:12 +0300

    Add files via upload

---
+++ Business/Abstract/IApplicantService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+
+
+namespace Business.Abstract
+{
+    public interface IApplicantService
+    {
+        public void ApllyForMask(Person person);
+
+
+        public List<Person> GetList();
+
+
+        public bool CheckPerson(Person person);
+
+    }
+}
---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IApplicantService _applicantservice;
+
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicantservice = applicantService;
+                
+        }
+        public void GiveMask(Person person)
+        {
+            
+            if (_applicantservice.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " Maske Verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " MASKE VERİLEMEDİ");
+            }
+        }
+    }
+}
